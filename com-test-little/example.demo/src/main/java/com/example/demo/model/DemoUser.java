742bcac8071a05d560f63f624a064209c2942a65
zhujiajun777 <1038642703@qq.com>
2020-08-04 17:07:53 +0800

    spring整合mybatis

--- com-test-little/example.demo/src/main/java/com/example/demo/model/DemoUser.java
+++ com-test-little/example.demo/src/main/java/com/example/demo/model/DemoUser.java
@@ -10,49 +10,12 @@ public class DemoUser {
 
     private String code;
 
-    private String name;
+    private String userName;
+
+    private String password;
 
     private String status;
 
-    private Date updatetime;
+    private Date updateTime;
 
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getCode() {
-        return code;
-    }
-
-    public void setCode(String code) {
-        this.code = code == null ? null : code.trim();
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name == null ? null : name.trim();
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status == null ? null : status.trim();
-    }
-
-    public Date getUpdatetime() {
-        return updatetime;
-    }
-
-    public void setUpdatetime(Date updatetime) {
-        this.updatetime = updatetime;
-    }
 }

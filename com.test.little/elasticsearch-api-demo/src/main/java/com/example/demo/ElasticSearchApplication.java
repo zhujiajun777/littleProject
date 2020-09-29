a1d6c7020774995f8d8e25bf72c3fae063229458
zhujiajun777 <1038642703@qq.com>
2020-09-29 15:40:29 +0800

    update elasticsearch

---
+++ com.test.little/elasticsearch-api-demo/src/main/java/com/example/demo/ElasticSearchApplication.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * TODO: ElasticSearch 启动类
+ * @author zhujiajun
+ * @date 2020/9/7 14:19
+ */
+@SpringBootApplication
+public class ElasticSearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElasticSearchApplication.class,args);
+    }
+
+}
